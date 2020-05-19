2f4edd7273fbde1937ba663a2902d1bfa4ac1f45
Supriya Das <41727446+supriyadas13@users.noreply.github.com>
2020-05-19 00:17:20 -0700

    first commit

---
+++ spring-boot-datastore/src/main/java/com/supriya/spring/boot/datastore/SpringBootDatastoreApplication.java
@@ -0,0 +1,13 @@
+package com.supriya.spring.boot.datastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatastoreApplication.class, args);
+	}
+
+}
---
+++ spring-boot-datastore/src/test/java/com/supriya/spring/boot/datastore/SpringBootDatastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supriya.spring.boot.datastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
